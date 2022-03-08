1fe46420c573aeba3b1f0ad5423cdadb2998e4af
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-03-08 19:32:05 +0300

    commit2 08 03 2022 задача в том чтобы одно карточка(№2) не могла быть использована одновременно в разных банкоматах

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,23 @@
+import configuration.AppContextConfiguration;
 import entity.ATM;
 import entity.Card;
 import entity.Person;
 
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import service.ATMService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException{
-//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppContextConfiguration.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppContextConfiguration.class);
 
-        ATM atm1 = new ATM(1,9000,true);
-        ATM atm2 = new ATM(2,6500,true);
-        ATM atm3 = new ATM(3,5600,true);
 
-        Person person1 = new Person(1,"Leonid", "Litvin");
-        Person person2 = new Person(2,"Nikolay", "Litvin");
-        Person person3 = new Person(3,"Valentina", "Litvin");
-
-        Card card1 = new Card("Visa", "1234-5678-9876-5432", 8000);
-        Card card2 = new Card("Visa", "0987-6543-2112-3456", 4500);
-
-        person1.setCard(card2);
-        person2.setCard(card2);
-        person3.setCard(card1);
-        card2.setPerson(person1);
-        card1.setPerson(person3);
-        ATMService atmService1 = new ATMService(atm1, card1);
-        Thread thread1 = new Thread(atmService1);
+        Thread thread1 = new Thread((Runnable) applicationContext.getBean("atmService1"));
         thread1.start();
-        ATMService atmService2 = new ATMService(atm2, card2);
-        Thread thread2 = new Thread(atmService2);
+
+        Thread thread2 = new Thread((Runnable) applicationContext.getBean("atmService2"));
         thread2.start();
-        ATMService atmService3 = new ATMService(atm3, card2);
-        Thread thread3 = new Thread(atmService3);
+
+        Thread thread3 = new Thread((Runnable) applicationContext.getBean("atmService3"));
         thread3.start();
-//        applicationContext.close();
     }
 }
--- src/configuration/AppContextConfiguration.java
+++ src/configuration/AppContextConfiguration.java
@@ -4,28 +4,65 @@ package configuration;
 import entity.ATM;
 import entity.Card;
 import entity.Person;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Scope;
-//
-//@Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import service.ATMService;
+
+@Configuration
 public class AppContextConfiguration {
-//
-//    @Scope("prototype")
-//    @Bean
-//    public Person person(Card card) {
-//        return new Person(card);
-//    }
-//
-//    @Scope("prototype")
-//    @Bean
-//    public Card card(Person person) {
-//        return new Card(person);
-//    }
-//
-//    @Scope("prototype")
-//    @Bean
-//    public ATM atm(Card card) {
-//        return new ATM(card);
-//    }
+
+    @Bean
+    public ATM atm1() {
+        return new ATM(1, 9000, true);
+    }
+
+    @Bean
+    public ATM atm2() {
+        return new ATM(2, 6500, true);
+    }
+
+    @Bean
+    public ATM atm3() {
+        return new ATM(3, 5600, true);
+    }
+
+    @Bean
+    public Person person1() {
+        return new Person(1, "Leonid", "Litvin");
+    }
+
+    @Bean
+    public Person person2() {
+        return new Person(2, "Nikolay", "Litvin");
+    }
+
+    @Bean
+    public Person person3() {
+        return new Person(3, "Valentina", "Litvin");
+    }
+
+    @Bean
+    public Card card1(Person person3) {
+        return new Card("Visa", "1234-5678-9876-5432", person3, 8000);
+    }
+
+    @Bean
+    public Card card2(Person person1) {
+        return new Card("Visa", "0987-6543-2112-3456", person1, 4500);
+    }
+
+    @Bean
+    public ATMService atmService1(ATM atm1, Card card1) {
+        return new ATMService(atm1, card1);
+    }
+
+    @Bean
+    public ATMService atmService2(ATM atm2, Card card2) {
+        return new ATMService(atm2, card2);
+    }
+
+    @Bean public ATMService atmService3(ATM atm3, Card card2) {
+        return new ATMService(atm3, card2);
+    }
 }
